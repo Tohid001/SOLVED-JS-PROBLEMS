d427fee281d2356adb42a4b367632f0a6015ca5b
Tohidul Islam <mdtohid.sust@gmail.com>
2021-03-28 19:22:54 +0600

    snail problem solved using loop!

---
+++ snailProblem.js
@@ -0,0 +1,17 @@
+var days = 0;
+var distancePassed = 0;
+
+function output(depth, climbUp, climbDown) {
+  while (distancePassed < depth) {
+    if (distancePassed + climbUp > depth) {
+      days += 1;
+      distancePassed += climbUp;
+    } else {
+      distancePassed += climbUp - climbDown;
+      days += 1;
+    }
+  }
+
+  return days;
+}
+console.log(output(31, 7, 2));
