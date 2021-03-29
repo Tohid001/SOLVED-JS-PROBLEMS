0df81381a7a6ea7bc1bae35dd81fb1ad950d067d
Tohidul Islam <mdtohid.sust@gmail.com>
2021-03-30 03:42:35 +0600

    fixed the bug at line 9(=)

--- snailProblem.js
+++ snailProblem.js
@@ -1,9 +1,12 @@
+//Problem Definition: A snail climbs up 7 feet each day and slips back 2 feet at night. How many days will it take the snail to get out of the of a well with the given depth?
+
 var days = 0;
+
 var distancePassed = 0;
 
 function output(depth, climbUp, climbDown) {
   while (distancePassed < depth) {
-    if (distancePassed + climbUp > depth) {
+    if (distancePassed + climbUp >= depth) {
       days += 1;
       distancePassed += climbUp;
     } else {
